e7db5c5136985eb0b41d0d1ec75a2e90ed1f49f1
Artem <artemk0b2396@gmail.com>
2023-07-17 18:30:18 +0300

    header

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 	</a>
 
 	<?php get_template_part( 'template-parts/header/site-header' ); ?>
-
+	
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
